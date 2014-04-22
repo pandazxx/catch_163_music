88c65c82fec5b2d80fc5eb859fc3c467f4caebfd
Xiangxin Zheng <pandazxx@gmail.com>
2014-04-22 23:55:06 +0800

    Login works

--- login.py
+++ login.py
@@ -13,25 +13,38 @@ def login(username, password):
                  'rememberLogin': 'true'}
     headers = {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Referer': 'http://music.163.com/',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36',
+      "Accept": "*/*",
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1)"
+                    " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
+      "Accept-Language": "zh-cn",
+      "Accept-Encoding": "gzip;deflate",
+      "Connection": "keep-alive",
+      "Referer": "http://music.163.com/"
     }
-    data = 'username=pandazxx%40163.com&password=a165968b0a8084a041aed89bf40d581f&rememberLogin=true'
 
-    resp = requests.post('http://music.163.com/api/login/?csrf_token=', data=data, headers=headers)
+    post_data = {
+      'username': username,
+      'password': __getmd5(password),
+      'rememberLogin': 'true'
+    }
 
-    print resp
-    print resp.text
-    print resp.cookies
+    # data = 'username=pandazxx%40163.com&password=a165968b0a8084a041aed89bf40d581f&rememberLogin=true'
 
-    playlist = requests.get('http://music.163.com/api/user/playlist/', headers=headers, cookies=resp.cookies)
+    resp = requests.post('http://music.163.com/api/login', data=post_data, headers=headers)
 
-    print playlist
-    print playlist.text
+    return resp
+    # print(resp)
+    # print resp.text
+    # print resp.cookies
+    #
+    # playlist = requests.get('http://music.163.com/api/user/playlist/', headers=headers, cookies=resp.cookies)
+    #
+    # print playlist
+    # print playlist.text
 
 
 def main():
-    print login('xx', 'xxx')
+    # print login('xx', 'xxx')
     pass
 
 if __name__ == '__main__':
