0ab795197d2bb3d76cc23bc97bc44b210c029746
zhengxx <zhengxx@ucweb.com>
2014-04-22 23:31:21 +0800

    Try using requests module

--- login.py
+++ login.py
@@ -1,25 +1,33 @@
-from urllib import urlencode
-import cookielib
-import urllib2
 import hashlib
+import requests
 
 
 def __getmd5(src):
     return hashlib.new('md5', src).hexdigest()
     pass
 
-def login(username, password):
-    cj = cookielib.LWPCookieJar() 
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj)) 
-    urllib2.install_opener(opener) 
-    user_data = {'username': username, 
-                'password': __getmd5(password), 
-                'rememberLogin':'true'
-                }
-    url_data = urlencode(user_data) 
 
-    login_r = opener.open('http://music.163.com/api/login/', url_data)
-    return login_r.read()
+def login(username, password):
+    user_data = {'username': username,
+                 'password': __getmd5(password),
+                 'rememberLogin': 'true'}
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Referer': 'http://music.163.com/',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36',
+    }
+    data = 'username=pandazxx%40163.com&password=a165968b0a8084a041aed89bf40d581f&rememberLogin=true'
+
+    resp = requests.post('http://music.163.com/api/login/?csrf_token=', data=data, headers=headers)
+
+    print resp
+    print resp.text
+    print resp.cookies
+
+    playlist = requests.get('http://music.163.com/api/user/playlist/', headers=headers, cookies=resp.cookies)
+
+    print playlist
+    print playlist.text
 
 
 def main():
