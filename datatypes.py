eb7a114c7df417265ca41be3282e2d337b8c59ea
Xiangxin Zheng <pandazxx@gmail.com>
2014-04-27 23:34:34 +0800

    Add DictData, convert dict to data object

--- datatypes.py
+++ datatypes.py
@@ -6,6 +6,48 @@ __author__ = 'pandazxx'
 import json
 
 
+class DictData(object):
+    def __init__(self, dict={}):
+        if not dict or len(dict) == 0:
+            return
+        required_attr_names = [x for x in dir(self) if not x.startswith('_')]
+        for attr_name in required_attr_names:
+            attr = getattr(self, attr_name)
+            if callable(attr) or isinstance(attr, type):
+                continue
+            if not attr_name in dict:
+                raise AttributeError('Required attribute "{0}" not found'.format(attr_name))
+            value = dict[attr_name]
+            if isinstance(attr, DictData):
+                if not isinstance(value, type(dict)):
+                    raise TypeError('Wrong type of value found when building <{class_name}> attribute "{attr_name}", '
+                                    'required <{required_type}>, '
+                                    'found <{real_type}>'.format(class_name=DictData.__name__,
+                                                                attr_name=attr_name,
+                                                                required_type=type(dict).__name__,
+                                                                real_type=type(value).__name__))
+                self.__dict__[attr_name] = DictData(value)
+            else:
+                self.__dict__[attr_name] = value
+        for remaining_attr_name in dict.keys() - required_attr_names:
+            if remaining_attr_name.startswith('_'):
+                continue
+            self.__dict__[remaining_attr_name] = dict.get(remaining_attr_name, None)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def to_dict(self):
+        props = self.__dict__.copy()
+        for key, value in props.items():
+            if isinstance(value, DictData):
+                props[key] = value.to_dict()
+        return props
+
+
 class StdData(dict):
     def __init__(self, **kwargs):
 
@@ -62,3 +104,82 @@ class StdData(dict):
             props[key] = value
 
         return props
+
+
+def main():
+    class test_cls1(DictData):
+        required1 = None
+        required2 = DictData()
+        required3 = {}
+        def not_required1(self): print("not_required1")
+        def _not_required2(self): print("_not_required2")
+        __not_required3 = '__not_required3'
+        def test_desc(self):
+            t1 = self
+            print('required1: {0}'.format(str(self.required1)))
+            print('required2: {0}'.format(str(self.required2)))
+            print('required3: {0}'.format(str(self.required3)))
+            print('not_required1: {0}'.format(str(self.not_required1)))
+            print('_not_required2: {0}'.format(str(self._not_required2)))
+            print('__not_required3: {0}'.format(str(self.__not_required3)))
+            print('dict: {0}'.format(str(self.to_dict())))
+
+    dict1 = {
+        'required1': 'required1_val',
+        'required2': {'extra1': 'extra1_val'},
+        'required3': {'required3': 'required3_val'},
+        'not_required1': "not_required1_val",
+        '_not_required2': "_not_required2_val",
+        '__not_required3': '__not_required3_val',
+        'extra2': 'extra2_val',
+        '_extra3': 'extra3_val',
+    }
+
+    dict2 = {
+        'required1': 'required1_val',
+        'required2': 'required2_val',
+        'required3': {'required3': 'required3_val'},
+        'not_required1': "not_required1_val",
+        '_not_required2': "_not_required2_val",
+        '__not_required3': '__not_required3_val',
+        'extra2': 'extra2_val',
+    }
+
+    dict3 = {
+        'required2': {'extra1': 'extra1_val'},
+        'required3': {'required3': 'required3_val'},
+        'not_required1': "not_required1_val",
+        '_not_required2': "_not_required2_val",
+        '__not_required3': '__not_required3_val',
+        'extra2': 'extra2_val',
+    }
+    try:
+        t1 = test_cls1(dict1)
+        t1.test_desc()
+        print('t1.required2.extra1: {0}'.format(t1.required2.extra1))
+    except Exception as e:
+        import traceback
+        print(e)
+        traceback.print_exc()
+    try:
+        t1 = test_cls1(dict2)
+        t1.test_desc()
+        print('t1.required2.extra1: {0}'.format(t1.required2.extra1))
+    except Exception as e:
+        import traceback
+        print(e)
+        traceback.print_exc()
+    try:
+        t1 = test_cls1(dict3)
+        t1.test_desc()
+        print('t1.required2.extra1: {0}'.format(t1.required2.extra1))
+    except Exception as e:
+        import traceback
+        print(e)
+        traceback.print_exc()
+
+
+    pass
+
+if __name__ == '__main__':
+    main()
